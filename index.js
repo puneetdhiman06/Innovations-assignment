218c5ec5f13296987ef052c812e4ac92d6979475
puneet <puneetdhiman.06@gmail.com>
2022-12-22 18:48:11 +0530

    uploading

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.listen(8000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server Started Successfully.");
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://puneet8500:puneet8500@cluster0.a4zc4xe.mongodb.net/merstack?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+app.use(express.json());
+app.use("/", authRoutes);
